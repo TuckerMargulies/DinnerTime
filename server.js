45788e82737991b899747ebde805040f55c39bd3
Tucker Margulies <marguliw@gmail.com>
2017-04-05 16:51:44 -0400

    editing

--- server.js
+++ server.js
@@ -37,22 +37,22 @@ server.use(expressEjsLaouts)
 server.use(methodOverride("_method"))
 
 
-server.get('/', function (req, res, next){
-  res.render('new')
-})
-// / home screen /////
 // server.get('/', function (req, res, next){
-//   Dinner.find( function (err, foundRecipe) {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log("foundRecipe " + foundRecipe)
-//       res.render('home', {
-//         Recipe:foundRecipe
-//       })
-//     }
-//   })
-// })
+//   res.render('new')
+})
+
+server.get('/', function (req, res, next){
+  Dinner.find( function (err, foundRecipe) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("foundRecipe " + foundRecipe)
+      res.render('home', {
+        Recipe:foundRecipe
+      })
+    }
+  })
+})
 
 
 //// render recipe entry form /////
